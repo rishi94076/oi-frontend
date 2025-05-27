ed83c5038ac803d494e042a7a8687aab5c77ac2c
rishi94076 <105619683+rishi94076@users.noreply.github.com>
2025-05-27 11:24:10 +0530

    Update oi.js

--- api/oi.js
+++ api/oi.js
@@ -1,10 +1,8 @@
-export default async function handler(req, res) {
-  // Dummy data for now
+export default function handler(req, res) {
   const data = {
     support: 22000,
     resistance: 22500,
     signal: "Buy",
   };
-
   res.status(200).json(data);
 }
