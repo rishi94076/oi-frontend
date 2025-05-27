8474678011f663659be5ce29683128b0fced9d43
rishi94076 <105619683+rishi94076@users.noreply.github.com>
2025-05-27 11:30:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
-  return <h1>hi</h1>;
+  return <h1> OI Chain is Running! </h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
